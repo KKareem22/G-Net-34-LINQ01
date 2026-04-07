c4e9efa33226c07b949b8c02f97797f8ff6b4425
Kareem <krda4969@gmail.com>
2026-04-07 15:14:02 +0200

    Q01

--- G-Net-34-LINQ01/Program.cs
+++ G-Net-34-LINQ01/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace G_Net_34_LINQ01
+﻿using G_Net_34_LINQ01.Models;
+using static G_Net_34_LINQ01.DataSources.Source;
+namespace G_Net_34_LINQ01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            ////Get all products from the "Seafood" category. Print each product's name and price.
+            //var query = ProductList
+            //    .Where(p => p.Category == "Seafood")
+            //    .Select(p =>$"ProductName :{p.ProductName} | Price : {p.UnitPrice}  ");
+            //Helper.Print("Q1", query);
+            #endregion
+
         }
     }
 }
